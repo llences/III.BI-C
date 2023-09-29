dec8ffde11e1627cdee178b7f828d852e9a0a4fc
llences <lukolences@gmail.com>
2023-09-29 09:16:56 +0200

    III.BÍ 1.den

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("zadajte cislo k dvojnasobeniu: ");
+string s = Console.ReadLine();
+int a = int.Parse(s);
+a = a * 2;
+Console.WriteLine(a);
+Console.ReadKey();
+
+
---
+++ string nasobenie.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Vytajte v kalkulacke");
+Console.WriteLine("zadajte prve cislo ");
+string s = Console.ReadLine();
+float a= int.Parse(s);
+Console.WriteLine("zadajte druhe cislo");
+string c = Console.ReadLine();
+float b = int.Parse(c);
+
+float sucin, sucet, rozdiel, podiel;
+
+sucet = a * b;
+Console.WriteLine("sucet: " + sucet);   
+
+sucin = a + b;
+Console.WriteLine("sucin: " + sucin);
+
+rozdiel = a - b;
+Console.WriteLine("rozdiel: " + rozdiel);
+
+podiel = a / b;
+Console.WriteLine("podiel: "+ podiel);
+Console.ReadKey();
+
+
